359281a8a6f1dc33a07fd705fb2c2d31fc458789
Jana Liebke <robsonliebke@gmail.com>
2021-11-20 17:22:14 +0100

    some types improvements

--- src/tree.utils.ts
+++ src/tree.utils.ts
@@ -2,8 +2,40 @@ export class TreeUtils {
 	public static treeFrom<T, K extends keyof T>(
 		array: T[],
 		options: {
-			keyProperty: K;
-			parentKeyProperty: K;
+			idPropertyName: K;
+			parentIdPropertyName: K;
 		}
-	) {}
+	) {
+		type U = T & { children: U[] };
+
+		const root: U = {
+			children: []
+		} as any;
+
+		if (array) {
+			const idPropertyName = options.idPropertyName;
+			const parentIdPropertyName = options.parentIdPropertyName;
+
+			const nodesByKey: Map<T[K], T> =
+				array.reduce((previous, current) => {
+					const key = current[idPropertyName];
+					(current as any).children = [];
+					previous.set(key, current);
+					return previous;
+				}, new Map());
+
+			for (const node of array) {
+				const currentParentKey = node[parentIdPropertyName];
+				const currentKey = node[idPropertyName];
+				const currentNode = nodesByKey.get(currentKey);
+				if (currentParentKey) {
+					const currentParentNode = nodesByKey.get(currentParentKey);
+					(currentParentNode as any).children.push(currentNode);
+					continue;
+				}
+				root.children.push(currentNode as any);
+			}
+		}
+		return root;
+	}
 }
