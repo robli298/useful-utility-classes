e243ca4a93c8b8c9bdc6c4ba7f8f2d41e7411b94
Robson Liebke <liebke@media-interactive.de>
2021-11-28 01:18:12 +0100

    add some tests

--- src/utils.test.ts
+++ src/utils.test.ts
@@ -1,21 +1,49 @@
 import { Utils } from './utils';
 
-it('', () => {
+jest.useFakeTimers();
 
-    class Test {
-		name: string = 'Rob';
-		constructor() {}
-		fn(a: string) {
-			console.log(`${this.name} - ${a}`);
+describe('debounce', () => {
+	let func: jest.Mock;
+	let debouncedFunc: Function;
+
+	beforeEach(() => {
+		func = jest.fn();
+	});
+
+	test('execute just once', () => {
+		debouncedFunc = Utils.debounce(func, 1000);
+
+		for (let i = 0; i < 100; i++) {
+			debouncedFunc();
 		}
-	}
 
+		// Fast-forward time
+		jest.runAllTimers();
 
-    const t = new Test();
+		expect(func).toBeCalledTimes(1);
+	});
 
-	t.fn = Utils.debounce(t.fn, 100, false);
-    t.fn('2')
+	test('execute just two times', () => {
+		debouncedFunc = Utils.debounce(func, 1000, true);
 
-    expect(true).toEqual(true);
+		for (let i = 0; i < 2; i++) {
+			debouncedFunc();
+		}
 
+		jest.runAllTimers();
+
+		expect(func).toBeCalledTimes(2);
+	});
+
+	test('wait value is zero if omitted', () => {
+		debouncedFunc = Utils.debounce(func);
+
+		for (let i = 0; i < 100; i++) {
+			debouncedFunc();
+		}
+
+		jest.runAllTimers();
+
+		expect(func).toBeCalledTimes(1);
+	});
 });
--- src/utils.ts
+++ src/utils.ts
@@ -1,15 +1,17 @@
 export class Utils {
-	public static debounce(func: Function, wait: number, immediate: boolean = false) {
+	public static debounce(func: Function, wait: number = 0, immediate: boolean = false) {
 		let timeout: any;
-		return function (this: any, args: any = '') {
+		return function (this: any) {
 			if (!immediate) {
 				clearInterval(timeout);
 				timeout = setTimeout(() => {
-					func.apply(this, [...args]);
+					func.apply(this, arguments);
+					timeout = null;
 				}, wait);
 				return;
 			}
-			func.apply(this, [...args]);
+
+			func.apply(this, arguments);
 		};
 	}
 }
